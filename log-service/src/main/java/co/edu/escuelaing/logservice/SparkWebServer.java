2175abb634887aae7e69c3f6d000183db290578d
JulianBenitez <julian.benitezg99@gmail.com>
2022-10-11 22:36:59 -0500

    Log service implemented

---
+++ log-service/src/main/java/co/edu/escuelaing/logservice/SparkWebServer.java
@@ -0,0 +1,19 @@
+package co.edu.escuelaing.logservice;
+
+import co.edu.escuelaing.logservice.controller.LogController;
+import co.edu.escuelaing.logservice.factories.impl.ControllerFactory;
+
+import static co.edu.escuelaing.logservice.config.Config.getPort;
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+public class SparkWebServer {
+    public static void main(String... args) {
+        LogController controller = ControllerFactory.instance.getInstance();
+        port(getPort());
+        get("hello", (req, res) -> "Hello!");
+        post("save", controller::save);
+    }
+
+}
